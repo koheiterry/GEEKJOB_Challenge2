013e5c8bc643a66c889141b3f2df8d7427015b27
LAPTOP-RDR9VVD7\ahk2s <k.oshima.sc@gmail.com>
2018-04-10 17:39:25 +0900

    体験課題初級３

---
+++ CampChallenge_PHP/000.基礎/taiken3.php
@@ -0,0 +1,27 @@
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>世界のナベアツ</title>
+</head>
+<body>
+    <h1>世界のナベアツ</h1>
+       <?php
+        //　----------ここから下へソースコードを書く------------
+         $num = 1;
+         while($num<=40){
+           echo $num;
+           if($num%3 == 0 || strpos($num,'3') !==false){
+             echo "あほになる";
+           }
+           if($num%5 == 0){
+             echo "犬っぽくなる";
+           }
+           echo "<br>";
+           $num++;
+         }
+       //　-------------------------ここまで------------------------------
+
+        ?>
+</body>
+</html>
