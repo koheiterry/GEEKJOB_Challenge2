4d465966490afff570d4a2199b3bfaacb664fef3
LAPTOP-RDR9VVD7\ahk2s <k.oshima.sc@gmail.com>
2018-04-19 21:56:05 +0900

    引数、戻り値

---
+++ CampChallenge_java/011.メソッド/NewServlet8.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.serviet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ahk2s
+ */
+public class NewServlet8 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+   String[] getProfile(int id) {
+        String[][] profiles = {
+            {"0", "中居正広", "1972年8月18日", "神奈川県藤沢市"},
+            {"1", "草彅 剛", "1974年7月9日", null},
+            {"2", "香取慎吾", "1977年1月31日", "神奈川県横浜市"}
+        };
+
+for (String[] profile: profiles) {
+int profileId = Integer.parseInt(profile[0]);
+if (id == profileId) {
+return profile;
+}
+}
+return null;
+}
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+                String[] labels = {"ID", "名前", "生年月日", "住所"};
+                Integer limit = 2;
+                for (int i = 0; i < 3; i++) {
+                    if (i >= limit) {
+                        break;
+                    }
+                    String[] profile = getProfile(i);
+                    
+                    for (int j = 1; j < profile.length; j++) {
+                        if (profile[j] == null) {
+                            continue;
+                        }
+                        out.print(labels[j] + ": " + profile[j] + "<br>");
+                    }
+                }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
