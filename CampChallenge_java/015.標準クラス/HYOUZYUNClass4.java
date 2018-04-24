a82d9409d6d1942bceba1d1e0a634174765e2321
LAPTOP-RDR9VVD7\ahk2s <k.oshima.sc@gmail.com>
2018-04-24 22:54:27 +0900

    バイト数と文字数の取得

---
+++ CampChallenge_java/015.標準クラス/HYOUZYUNClass4.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet;
+
+/**
+ *
+ * @author ahk2s
+ */
+public class HYOUZYUNClass4 {
+    public static void main(String[] args) {
+        String word = "大島滉平";
+        System.out.println(word.length());
+        System.out.println(word.getBytes().length);
+    }
+}
