9c1040ee2ae8900a90ecb1be353a8ab407696c7c
LAPTOP-RDR9VVD7\ahk2s <k.oshima.sc@gmail.com>
2018-04-24 22:54:40 +0900

    文字数の取得

---
+++ CampChallenge_java/015.標準クラス/HYOUZYUNClass5.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet;
+
+/**
+ *
+ * @author ahk2s
+ */
+public class HYOUZYUNClass5 {
+    public static void main(String[] args) {
+        String word = "k.oshima@gmail.com";
+        System.out.println(word.substring(8));
+    }
+}
